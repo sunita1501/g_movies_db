558fa9f4d5ef9da3677da4b78d935521193640f8
Prasad Nettem <rajprasad.nettem@gmail.com>
2021-03-20 21:43:39 +0530

    green getmovies endpoint

--- src/main/java/com/gmovies/GMovieDB.java
+++ src/main/java/com/gmovies/GMovieDB.java
@@ -1,13 +1,16 @@
 package com.gmovies;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@RestController
 public class GMovieDB {
 
     List<String> movieList = new ArrayList<String>();
 
-
+@GetMapping("movies")
     public List<String> getMovies() {
 
 
---
+++ src/test/java/com/gmovies/GmovieDBIT.java
@@ -0,0 +1,26 @@
+package com.gmovies;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GmovieDBIT {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void getmoviesTest()throws Exception{
+        mockMvc.perform(get("/movies").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+    }
+}
