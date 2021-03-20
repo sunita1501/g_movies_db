5f1bff8f35b1e06b17a999abd57d3ecde2d22178
Prasad Nettem <rajprasad.nettem@gmail.com>
2021-03-21 00:50:44 +0530

    red on movie rating

--- src/test/java/com/gmovies/GmovieDBIT.java
+++ src/test/java/com/gmovies/GmovieDBIT.java
@@ -50,4 +50,15 @@ public class GmovieDBIT {
         mockMvc.perform(get("/movie/Awesome2").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).
                 andExpect(jsonPath("$.title").value("Awesome2"));
     }
+    @Test
+    public void starRatingTest()throws Exception{
+        MovieDTO movieDTO1 =new MovieDTO("Awesome","Joe",0);
+        //MovieDTO movieDTO2 =new MovieDTO("Awesome2","Joe2",0);
+       // MovieDTO movieDTO3 =new MovieDTO("Awesome3","Joe3",0);
+        mockMvc.perform(post("/movie").content(objectMapper.writeValueAsString(movieDTO1)).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+        //mockMvc.perform(post("/movie").content(objectMapper.writeValueAsString(movieDTO2)).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+      //  mockMvc.perform(post("/movie").content(objectMapper.writeValueAsString(movieDTO3)).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+        mockMvc.perform(get("/movie/Awesome2").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).
+                andExpect(jsonPath("$.rating").value(0));
+    }
 }
