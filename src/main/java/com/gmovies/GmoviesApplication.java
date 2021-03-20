28004ffd0ea3be4462b9fa42963064174fdaa18b
zaneagley <zaneagley@gmail.com>
2021-03-20 09:58:50 -0500

    First initial commit

---
+++ src/main/java/com/gmovies/GmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.gmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmoviesApplication.class, args);
+	}
+
+}
